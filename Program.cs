574d1a6bd86c627fedadff252ff191d9acea52eb
Ejackson9 <91967987+Ejackson9@users.noreply.github.com>
2021-11-09 16:59:38 -0500

    Add files via upload
    Update Program

--- Program.cs
+++ Program.cs
@@ -7,42 +7,90 @@ namespace DriveRatingApp
 
     class Program
     {
-     
+
 
         static void Main(string[] args)
         {
-            Console.Write("Enter your CommonID:");
-            string CommonId = Console.ReadLine();
-            string firstLetter = CommonId.Substring(0, 1);
-
 
             List<TeamMember> teamMembers = new List<TeamMember>();
 
-            teamMembers.Add(new TeamMember("Joe", "Spacito", "t1234", DriveRating.AchievingExpectations));
-            teamMembers.Add(new TeamMember("Jane", "Carrie", "t1235", DriveRating.AchievingExpectations));
-            teamMembers.Add(new TeamMember("Praj", "Nahim", "t1236", DriveRating.AchievingExpectations));
-            teamMembers.Add(new Leader("Fitz", "Caldwell", "l2239", DriveRating.AchievingExpectations));
-            teamMembers.Add(new Leader("Leslie", "Wrightfield", "l3239", DriveRating.AchievingExpectations));
-            teamMembers.Add(new Director("Charlie", "Georgina", "d5538", DriveRating.AchievingExpectations));
-            foreach(var item in teamMembers)
+            List<TeamMember> listFromRepo = TeamMemberRepo.GetTeamMembers();
+
+            teamMembers = TeamMemberRepo.GetTeamMembers();
+
+
+            Console.Write("Enter your Common Id:");
+            string ID = Console.ReadLine();
+            string firstletter = ID.Substring(0, 1);
+
+            if (firstletter == "t")
+
             {
-                Console.WriteLine(item);
+                switch (ID)
+                {
+                    case "t1234":
+                        // bonus = TeamMember.GetBonus()
+                        Console.WriteLine(teamMembers[0].FirstName + " Your current Drive Rating is " + teamMembers[0].Rating +
+                            " and you will be recieving " + teamMembers[0].GetBonus() + " as a bonus");
+                        break;
+                    case "t1235":
+                        Console.WriteLine(teamMembers[1].FirstName + " Your current Drive Rating is " + teamMembers[1].Rating +
+                            " and you will be recieving " + teamMembers[1].GetBonus() + " as a bonus");
+                        break;
+                    case "t1236":
+                        Console.WriteLine(teamMembers[2].FirstName + " Your current Drive Rating is " + teamMembers[2].Rating +
+                            " and you will be recieving " + teamMembers[2].GetBonus() + " as a bonus");
+                        break;
+
+                }
             }
-            
-            if (firstLetter=="t")
+
+            if (firstletter == "l")
             {
-                              
+
+                Console.WriteLine("Would you like to(1)Update a Drive Rating or(2)View Bonus Report: ");
+                string leaderOptions = Console.ReadLine();
+                if (leaderOptions == "1")
+                {
+                    Console.Write("Enter TM Common Id:");
+                    string tmID = Console.ReadLine();
+                    string tm_firstletter = tmID.Substring(0, 1);
+                    if (tmID == "l" || tmID == "d")
+                    {
+                        Console.WriteLine("You do not have permissions to update leadership.");
+                    }
+                    // add loop around here; Console.WriteLine("Return to Leader Menu (1)Yes or (2)No: ")
+
+                    if (leaderOptions == "2")
+                    {
+                        switch (ID)
+                        {
+                            case "l2239":
+                                Console.WriteLine("Your DRIVE rating is " + teamMembers[3].Rating + " and your bonus is " + teamMembers[3].GetBonus());
+                                break;
+                            case "l3239":
+                                Console.WriteLine("Your DRIVE rating is " + teamMembers[4].Rating + " and your bonus is " + teamMembers[4].GetBonus());
+                                break;
+                        }
+                        Console.WriteLine("Team Members:");
+                        for (int i = 0; i <= 2; i++)
+                        {
+                            Console.WriteLine(teamMembers[i].LastName + "," + teamMembers[i].FirstName + " Drive Rating is " + teamMembers[i].Rating +
+                           " and their bonus is " + teamMembers[i].GetBonus());
+                        }
+
+                    }
+
+
+                }
+                if (firstletter == "d")
+                {
+
+                }
 
             }
-            if (firstLetter == "l")
-            {
-
-            }
-            if (firstLetter == "d")
-            {
-        
-            };
         }
-        
     }
 }
+        
+    
